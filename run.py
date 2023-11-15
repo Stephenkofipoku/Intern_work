db751d49da621a077eab84020d70b4b3740111e0
Stephen Opoku <stephen.opok@hotmail.com>
2023-11-15 19:44:51 +0000

    Add documents and adjust the codes

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
+import subprocess
+import sys
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,34 +14,27 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-appointments_spreadsheet = GSPREAD_CLIENT.open('p21_bi_intern_test_appointments')
-revenues_spreadsheet = GSPREAD_CLIENT.open('p21_bi_intern_test_revenues')
 
-# Access worksheets
-appointments_worksheet = appointments_spreadsheet.worksheet('appointments')
-revenues_worksheet = revenues_spreadsheet.worksheet('revenues')
+# Function to load data from Google Sheets
+def load_data(sheet_name, worksheet_name):
+    sheet = GSPREAD_CLIENT.open(sheet_name).worksheet(worksheet_name)
+    data = sheet.get_all_values()
+    headers = data[0]
+    values = data[1:]
+    df = pd.DataFrame(values, columns=headers)
+    return df
 
-# Get data from worksheet
-appointments_data = appointments_worksheet.get_all_values()
-revenues_data = revenues_worksheet.get_all_values()
+# Load data from Google Sheets
+appointments_df = load_data('p21_bi_intern_test_appointments', 'appointments')
+revenues_df = load_data('p21_bi_intern_test_revenues', 'revenues')
 
-# Convert data to pandas DataFrames
-appointments_df = pd.DataFrame(appointments_data[1:], columns=appointments_data[0])
-revenues_df = pd.DataFrame(revenues_data[1:], columns=revenues_data[0])
-
-# Replace "NULL" with NaN in 'revenue' column
-revenues_df['revenue'] = revenues_df['revenue'].replace("NULL", pd.NA)
-
-# Convert 'revenue' column to numeric
-revenues_df['revenue'] = pd.to_numeric(revenues_df['revenue'], errors='coerce')
-
-# Merge appointments and revenues on 'appointment_id'
+# Merge datasets
 merged_df = pd.merge(appointments_df, revenues_df, on='appointment_id', how='left')
 
-# Convert 'appointment_date' to datetime
-merged_df['appointment_date'] = pd.to_datetime(merged_df['appointment_date'])
+# Handle missing or null values in the revenue column
+merged_df['revenue'] = pd.to_numeric(merged_df['revenue'], errors='coerce')
 
-# Assuming the launch dates for clinics
+# Identify launch dates for each clinic
 launch_dates = {
     'clinic_1': pd.to_datetime('2022-01-01'),
     'clinic_2': pd.to_datetime('2022-01-01'),
@@ -46,44 +42,58 @@ launch_dates = {
     'clinic_4': pd.to_datetime('2023-07-01')
 }
 
-# Filter data for 2023 based on launch dates for all clinics
-data_2023 = merged_df.copy()
+# Extrapolate data for each clinic
+extrapolated_dfs = {}
 for clinic_id, launch_date in launch_dates.items():
-    data_2023.loc[data_2023['clinic_id'] == clinic_id, 'clinic_launch'] = launch_date
+    if clinic_id in ['clinic_1', 'clinic_2']:
+        # For clinics 1 and 2, extrapolate for the entire 2023
+        extrapolation_period = pd.date_range(start='2023-01-01', end='2023-12-31', freq='D')
+    else:
+        # For clinics 3 and 4, extrapolate from launch date to the end of 2023
+        extrapolation_period = pd.date_range(start=launch_date, end='2023-12-31', freq='D')
 
-# Filter data for 2023 based on clinic launch dates
-data_2023 = data_2023[data_2023['appointment_date'] >= data_2023['clinic_launch']]
+    # Create a template DataFrame for extrapolation
+    template_data = {
+        'appointment_id': [f'extrap_{i}' for i in range(1, len(extrapolation_period) + 1)],
+        'practitioner_id': ['extrap_practitioner'] * len(extrapolation_period),
+        'patient_id': ['extrap_patient'] * len(extrapolation_period),
+        'clinic_id': [clinic_id] * len(extrapolation_period),
+        'appointment_date': extrapolation_period,
+        'revenue': [0.0] * len(extrapolation_period)
+    }
 
-# Debugging prints
-print("Data Columns:")
-print(data_2023.columns)
+    # Create the extrapolated DataFrame based on the template
+    extrapolated_data = pd.DataFrame(template_data)
+    extrapolated_dfs[clinic_id] = extrapolated_data
 
-# Debugging print to check unique clinic IDs
-print("\nUnique Clinic IDs in Data for 2023:")
-print(data_2023['clinic_id'].unique())
+# Combine extrapolated data for all clinics
+extrapolated_all = pd.concat(extrapolated_dfs.values(), ignore_index=True)
+
+# Combine historical data and extrapolated data
+data_all = pd.concat([merged_df, extrapolated_all], ignore_index=True)
 
 # Calculate total revenue for each clinic
-total_revenue = data_2023.groupby('clinic_id')['revenue'].sum()
+total_revenue_by_clinic = data_all.groupby('clinic_id')['revenue'].sum()
 
-# Debugging prints
-print("\nData for 2023:")
-print(data_2023)
+# Count unique patients for all clinics
+unique_patients_all = data_all['patient_id'].nunique()
 
-print("\nTotal Revenue for Each Clinic in 2023:")
-print(total_revenue)
+# Print to the console
+print("\nEstimates for All Clinics in 2023:")
+print("Total Revenue by Clinic:")
+print(total_revenue_by_clinic.reset_index())  # Reset index to display clinic_id
+print(f"\nTotal Revenue: {total_revenue_by_clinic.sum()}")
+print(f"Unique Patients: {unique_patients_all}")
 
-# Count unique patients for each clinic
-unique_patients = data_2023.groupby('clinic_id')['patient_id'].nunique()
+# Redirect standard output to a text file
+output_file = 'output.txt'
+with open(output_file, 'w') as f:
+    # Use a context manager to capture the output
+    with redirect_stdout(f):
+        # Print to the file
+        print("\nEstimates for All Clinics in 2023:")
+        print("Total Revenue by Clinic:")
+        print(total_revenue_by_clinic.reset_index())  # Reset index to display clinic_id
+        print(f"\nTotal Revenue: {total_revenue_by_clinic.sum()}")
+        print(f"Unique Patients: {unique_patients_all}")
 
-# Sum up total revenue and unique patient counts for all clinics
-total_revenue_all = total_revenue.sum()
-total_unique_patients_all = unique_patients.sum()
-
-print("Total Revenue for Each Clinic in 2023:")
-print(total_revenue)
-
-print("\nUnique Patients for Each Clinic in 2023:")
-print(unique_patients)
-
-print("\nTotal Revenue for All Clinics in 2023:", total_revenue_all)
-print("Total Unique Patients for All Clinics in 2023:", total_unique_patients_all)
